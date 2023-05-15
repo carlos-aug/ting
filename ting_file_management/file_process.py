94dadfa4856ef4fe93a2fb6f9c7a185afb6fc065
Carlos Augusto <caarlos.san@gmail.com>
2023-05-15 20:26:22 -0300

    feat: add partial file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import os
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_name = os.path.basename(path_file)
+    if file_name in instance.items:
+        print(f"{file_name} já foi processado anteriormente.")
+        return
+
+    lines = txt_importer(path_file)
+    if not lines:
+        print(f"{path_file} não pode ser processado.")
+        return
+
+    file_dict = {
+        "nome_do_arquivo": file_name,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(file_name)
+    print(file_dict)
 
 
 def remove(instance):
