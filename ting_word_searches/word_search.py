c7401df2db5b9f071ecd6984f897167a06604eb7
Carlos Augusto <caarlos.san@gmail.com>
2023-05-15 22:19:45 -0300

    add exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    ...
+    result = []
+    for file_info in instance.items:
+        filename = txt_importer(file_info)
+        occurrences = []
+
+        for i, line in enumerate(filename):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i + 1})
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_info,
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
