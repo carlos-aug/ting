b48fd1c7d88c75f705d807ee8bef07cc7658006e
Carlos Augusto <caarlos.san@gmail.com>
2023-05-15 21:37:59 -0300

    refactor: def process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,31 +1,25 @@
-import os
 from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    file_name = os.path.basename(path_file)
-    if file_name in instance.items:
-        print(f"{file_name} já foi processado anteriormente.")
+    if path_file in instance.items:
         return
 
     lines = txt_importer(path_file)
-    if not lines:
-        print(f"{path_file} não pode ser processado.")
-        return
 
     file_dict = {
-        "nome_do_arquivo": file_name,
+        "nome_do_arquivo": path_file,
         "qtd_linhas": len(lines),
         "linhas_do_arquivo": lines,
     }
 
-    instance.enqueue(file_name)
+    instance.enqueue(path_file)
     print(file_dict)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    ...
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    ...
